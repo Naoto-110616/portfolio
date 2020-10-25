05b2d1ebee9b7fac9daf5495e945c3ec0d2b058a
Naoto-110616 <siodeitadaku@gmail.com>
2020-10-25 17:04:03 +0900

    add_web_pages

---
+++ Portfolio.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Portfolio</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header><h1><a href="Portfolio.php">Portfolio</a></h1></header>
+    <main>
+        <div class="content-wrapper">
+            <article class="experience">
+            <h2 class="Experience">Experience</h2>
+                <section class="elec-power">
+                    <table class="table" border="1">
+                        <thead class="thead">
+                            <tr>
+                                <th>案件</th>
+                                <th>期間</th>
+                                <th>言語</th>
+                                <th>担当フェーズ</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td><a href="skillsheet/skillsheet.php">Skillsheet</a></td>
+                                <td>8時間</td>
+                                <td>
+                                    <div>HTML</div>
+                                    <div>CSS</div>
+                                </td>
+                                <td>模写</td>
+                            </tr>
+                            <tr>
+                                <td><a href="goodbook/login.html">goodbook</a></td>
+                                <td>8時間</td>
+                                <td>
+                                    <div>HTML</div>
+                                    <div>CSS</div>
+                                </td>
+                                <td>模写</td>
+                            </tr>
+                            <tr>
+                                <td>自習</td>
+                                <td>-</td>
+                                <td>
+                                    <div>HTML/CSS</div>
+                                    <div>PHP</div>
+                                    <div>javascript</div>
+                                    <div>jQuaery</div>
+                                    <div>SQL</div>
+                                </td>
+                                <td>模写</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </section>
+            </article>
+            <h2>Wolks</h2>
+            <section class="item">
+                <h3>goodbook</h3>
+                <div class="icon">
+                    <a href="goodbook/login.html"><img src="goodbook/img/Facebook-Logo1.png" alt="goodbook_icon"></a>
+                </div>
+                <p>HTML,CSSのサンプルです．facebookのレイアウトを模写しました.</p>
+                <h3>Skills</h3>
+                <ul>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                </ul>
+            </section>
+        </div>
+    </main>
+    <footer class="footer">
+    <div class="content">
+    <h2>Portfolio</h2>
+        <p>お時間いただき、ありがとうございました。</p>
+        <div>これまでの経験を活かして、業務を進めて参ります。</div>
+        <div>よろしくお願いします。</div>
+        <p><a href="Portfolio.php">Portfolio トップページへ</a></p>
+        </div>
+    </footer>
+</body>
+</html>
---
+++ skillsheet/skillsheet.php
@@ -0,0 +1,204 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Skillsheet</title>
+    <link rel="stylesheet" href="../style.css">
+</head>
+<body>
+    <header>
+        <h1><a href="skillsheet.php">skillsheet</a></h1>
+    </header>
+    <main>
+    <div class="content-wrapper">
+        <article class="experience">
+            <section class="elec-power">
+                <table class="table2" border="1">
+                    <tbody>
+                        <tr>
+                           <td class="color">ふりがな</td>
+                           <td>おおかわなおと</td>
+                            <td class="color">生年月日</td>
+                           <td>1999年6月</td>
+                            <td class="color">年齢</td>
+                           <td>21歳</td>
+                       </tr>
+                        <tr>
+                            <td class="color">氏名</td>
+                            <td>大川尚斗</td>
+                            <td class="color">経験年数</td>
+                            <td>3ヶ月</td>
+                            <td class="color">最寄駅</td>
+                            <td>
+                                <div class="lang">地下鉄谷町線</div>
+                                <div class="lang">長原駅</div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="color">現住所</td>
+                            <td>大阪市平野区長吉出戸</td>
+                            <td class="color">性別</td>
+                            <td>男</td>
+                            <td class="color">職種</td>
+                            <td>ITエンジニア</td>
+                        </tr>
+                        <tr>
+                            <td class="color">学歴</td>
+                            <td colspan="3">放送芸術学院専門学校 メディアクリエイト科 TV番組制作コース 卒業</td>
+                            <td class="color">通勤可能時間</td>
+                            <td>-</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </section>
+            <section class="elec-power">
+                <table class="table2" border="1">
+                   <thead>
+                       <tr>
+                           <th colspan="7" class="color">職務実績</th>
+                       </tr>
+                   </thead>
+                    <tbody>
+                        <tr>
+                            <td class="color">順番</td>
+                            <td colspan="2" class="color">期間</td>
+                            <td class="color">業務名/業種</td>
+                            <td class="color">   </td>
+                            <td class="color">言語/DB/ツール</td>
+                            <td class="color">
+                            <div>職位</div>
+                            <div>(メンバー数)</div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="color">1</td>
+                            <td>
+                                <div>2018/12</div>
+                                <div>-</div>
+                                <div>2020/3</div>
+                            </td>
+                            <td>1年3ヶ月</td>
+                            <td class="sentence">TV番組制作会社(準社員)<br>
+
+                                【業務内容】<br>
+                                ロケ準備業務:朝の情報番組「よーいどん」内の「隣の人間国宝さん」のロケに伴う全ての準備<br>
+                                台本作成に伴う情報収集：放送許可の取得やデータ収集<br>
+                                撮影した映像の処理に伴う事務：ダビングしたあとデータ化し、PCに取り込む。<br>
+                                編集しやすいように環境設定<br>
+                                特番に伴う、街頭アンケートや書類作成、取材業務<br>
+                                出演者の身の回りのケア業務<br>
+
+                                【取り組み・実績】<br>
+                                放送日が決まっているので、納期を必ず守らなければならず、また制作にかかわるメンバーが<br>
+                                非常に多いので、１つでも工程が遅れると全てに影響します。ですので時間の管理、<br>
+                                タスクの管理と処理、期日までの進捗管理徹底して行うよう心がけました。<br>
+
+                                その結果、働いていた期間1度も遅刻がなく、タスクに関して納期を過ぎたこと、忘れたこと、<br>
+                                タスクが漏れたことも1度もございませんでした。</td>
+                            <td>-</td>
+                            <td>-</td>
+                            <td>5名</td>
+                        </tr>
+                        <tr>
+                            <td class="color">2</td>
+                            <td>
+                                <div>2020/2</div>
+                                <div>-</div>
+                                <div>2020/5</div>
+                            </td>
+                            <td>3カ月</td>
+                            <td class="sentence">【オンライン学習ツールにて言語の勉強】<br>
+                                同居していた実兄がjavaエンジニアとして4年の経験者ですので、分からない事は聞きながら、<br>
+                                自宅でプログラム言語について習得しております。<br>
+
+                                【学習内容】<br>
+                                シフトシステムという会社の「1週間で身につくjava言語の基礎」<br>
+                                SQL書き方ドリルにて練習<br>
+                                研修ツール、「ドットインストール」を毎日</td>
+                            <td>Windows10</td>
+                            <td>
+                                <div>java</div>
+                                <div>SQL</div>
+                            </td>
+                            <td>-</td>
+                        </tr>
+                        <tr>
+                            <td class="color">3</td>
+                            <td>
+                                <div>2020/7</div>
+                                <div>-</div>
+                                <div>2020/12</div>
+                            </td>
+                            <td>6カ月</td>
+                            <td class="sentence">
+                                株式会社ダスキン（派遣）<br>
+                                【業務内容】<br>
+                                ミスタードーナツ店舗、事業部、工場のインターネット監視、お客様へ<br>
+                                機器の接続確認及び、説明、壊れた機器の交換手配。<br>
+
+                                【取り組み・実績】<br>
+                                対応は全て電話でのみの対応になるので、お客様へ具体的に理解しやすく<br>
+                                説明することを心がけました。<br>
+                                障害が起きない限りは、することが無いのでIT言語の勉強をする時間にしていました。</td>
+                            <td>-</td>
+                            <td>-</td>
+                            <td>8名</td>
+                        </tr>
+                        <tr>
+                            <td class="color">4</td>
+                            <td>
+                                <div>2020/7</div>
+                                <div>-</div>
+                                <div>現在</div>
+                            </td>
+                            <td>-</td>
+                            <td class="sentence">【オンライン学習ツールにて言語の勉強】<br>
+                                現在は平日３時間、休日4時間の学習をしております。<br>
+
+                                【学習内容】<br>
+                                ゼロワンという会社の「ウェブカツ」<br>
+                                HTML、CSS、PHP、javascript、jQuery、MySQLの練習</td>
+                            <td>
+                                <div>Mac</div>
+                                <div>Mojave</div>
+                            </td>
+                            <td>
+                                <div>HTML</div>
+                                <div>CSS</div>
+                                <div>PHP</div>
+                                <div>javascript</div>
+                                <div>jQuery</div>
+                                <div>MySQL</div>
+                            </td>
+                            <td>-</td>
+                        </tr>
+                        <tr>
+                            <td class= "sentence" colspan="7">【人物面】<br>
+                                ・とても真面目で、仕事に対して非常に根気強く向き合う人間です。真冬に朝まで街頭アンケートもしておりました。<br>
+                                　取材やアンケート、許可取得でも様々な方との折衝業務もこなしてきましたのでコミュニケーション能力もとても高く、また21歳とはおもえないほど<br>
+                                　社会人としてのマナーや話し方がしっかり備わっております。<br>
+
+                                ・またかなり勤勉で、毎日ひたすら分からないことを調べたり聞いたりと、コツコツ努力することもできております。<br>
+                                ・実兄がエンジニアということもあり、基礎からしっかりと教えてもらえる環境もあり、しっかり活用しながらIT言語を習得していっております。<br>
+
+                                【将来のエンジニア像】<br>
+                                　家族の環境的にjavaやSQLから勉強しておりますが、しっかりとITパスポートも勉強しており、将来は開発にこだわることなく<br>
+                                　様々な業界に関わることができるエンジニアになりたいと考えております。</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </section>
+        </article>
+    </div>
+    </main>
+    <footer class="footer">
+        <div class="content">
+            <h2>skillsheet</h2>
+             <p>お時間いただき、ありがとうございました。</p>
+                <div>これまでの経験を活かして、業務を進めて参ります。</div>
+            <div>よろしくお願いします。</div>
+            <p><a href="../Portfolio.php">Portfolio トップページへ</a></p>
+        </div>
+    </footer>
+</body>
+</html>
