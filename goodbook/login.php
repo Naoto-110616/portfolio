d7c1d26c1fabe2aab3c430ff14557059465822d3
Naoto-110616 <siodeitadaku@gmail.com>
2020-11-09 22:25:54 +0900

    files_organize

--- goodbook/login.php
+++ goodbook/login.php
@@ -119,7 +119,7 @@
         </div>
     </footer>
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-    <script src="main.js"></script>
+    <script src="js/main.js"></script>
 </body>
 
 </html>
