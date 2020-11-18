8b9857329180b8b39f450ccaea841892288e2c15
Naoto-110616 <siodeitadaku@gmail.com>
2020-11-18 23:20:31 +0900

    goodbook update #1

--- goodbook/homepage.php
+++ goodbook/homepage.php
@@ -1,13 +1,10 @@
 <?php
 
- //E_STRICTレベル以外のエラーを報告する
 error_reporting(E_ALL);
- //画面にエラーを表示させるか
 ini_set('display_errors', 'On');
 
 session_start();
 
-//ログインしてなければ、login画面へ戻す
 if (empty($_SESSION['login'])) {
     header("Location:login.php");
 }
@@ -22,10 +19,10 @@ if (empty($_SESSION['login'])) {
     <title>goodbook</title>
     <link rel="stylesheet" href="css/style_homepage.css">
     <link rel="stylesheet" href="css/reset.css">
+    <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
 </head>
 
 <body>
-
     <?php if (!empty($_SESSION['login'])) {
     ;
 }?>
@@ -37,17 +34,17 @@ if (empty($_SESSION['login'])) {
         </div>
         <div class="serch_div">
             <label>
-                <input type="serch" class="serch">
+                <input type="serch" class="serch" placeholder="goodbook search">
             </label>
         </div>
     </div>
     <div class="ul_div">
-        <ul class="ul">
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
+        <ul class="header_ul">
+            <li class="header_icon"><i class="fas fa-home fa-2x"></i></li>
+            <li class="header_icon"><i class=" fas fa-user-friends fa-2x"></i></li>
+            <li class="header_icon"><i class=" fas fa-tv fa-2x"></i></li>
+            <li class="header_icon"><i class=" fas fa-users fa-2x"></i></li>
+            <li class="header_icon"><i class=" fas fa-dice-d6 fa-2x"></i></li>
         </ul>
     </div>
 
