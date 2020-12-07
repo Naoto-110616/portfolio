d60dbddb476d66ced4583e7d74f797c9d7d9bc9b
Naoto-110616 <siodeitadaku@gmail.com>
2020-12-07 13:09:06 +0900

    fix homepage #1

---
+++ goodbook/js/homepage.js
@@ -0,0 +1,15 @@
+$(function(){
+    $('.js-menu__item__link').each(function(){
+        $(this).on('click',function(){
+            $("+.submenu",this).slideToggle();
+            $("i",this).toggleClass("fas fa-arrow-circle-down fa-2x");
+            $("i",this).toggleClass("fas fa-arrow-circle-up fa-2x");
+            if ($("span",this).text() === 'close') {
+                $("span",this).text("more");
+              } else {
+                $("span",this).text("close");
+              }
+            return false;
+        });
+    });
+});
