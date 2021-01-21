03f840736b7edb94c1d06470691a692834fcc341
busainu <siodeitadaku@gmail.com>
2021-01-21 15:39:05 +0900

    fix signup function #15

--- goodbook/signup.php
+++ goodbook/signup.php
@@ -7,47 +7,7 @@ debug("「 signup page");
 debug('「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「');
 debugLogStart();
 
-if (!empty($_POST)) {
-    debug('POST送信があります。');
-
-    $email = $_POST['email'];
-    $pass = $_POST['pass'];
-    $pass_re = $_POST['pass_re'];
-
-    validRequired($email, "email");
-    validRequired($pass, "pass");
-    validRequired($pass_re, "pass_re");
-
-    if (empty($err_msg)) {
-        debug('未入力チェックOK。');
-
-        validEmail($email, "email");
-        validMaxLen($email, "email");
-        validEmailDup($email, "email");
-
-        validPass($pass, "pass");
-        validPass($pass_re, "pass_re");
-
-        if (empty($err_msg)) {
-
-            validMatch($pass, $pass_re, "pass");
-
-            if (empty($err_msg)) {
-                debug('バリデーョンOKです。');
-
-                try {
-                    signUp($email, $pass, "email");
-                } catch (Exception $e) {
-                    error_log("error" . $e->getMessage());
-                    $err_msg[$key] = MSG09;
-                }
-            } else {
-                debug('バリデーョンNGです。');
-            }
-        }
-    }
-}
-
+signUp(isset($email), isset($pass), isset($pass_re), "email");
 
 debug('画面表示処理終了 <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<');
 ?>
@@ -66,7 +26,7 @@ require("head.php");
                     <form method="post">
                         <div class="emaildiv" action='login.php'>
                             <label for="email" class="<?php echo getErrMsglabel("email"); ?>">
-                                <input class="email" type="text" name="email" id="email" placeholder="Email" autofocus="1" value="<?php getFormData("email"); ?>">
+                                <input class="email" type="text" name="email" id="email" placeholder="Email" autofocus="1" value="<?php echo getFormData("email"); ?>">
                             </label>
                             <div class="help-block"></div>
                             <span class="err_msg"><?php echo getErrMsg("email"); ?>
@@ -74,7 +34,7 @@ require("head.php");
                         </div>
                         <div class="signupinput passworddiv">
                             <label for="password" class="<?php echo getErrMsglabel("pass"); ?>">
-                                <input class="password" type="password" name="pass" id="password" placeholder="Password" value="<?php getFormData("pass"); ?>">
+                                <input class="password" type="password" name="pass" id="password" placeholder="Password" value="<?php echo getFormData("pass"); ?>">
                             </label>
                             <div class="help-block"></div>
                             <span class="err_msg"><?php echo getErrMsg("pass");  ?>
@@ -82,7 +42,7 @@ require("head.php");
                         </div>
                         <div class="signupinput password_retypediv">
                             <label for="password_retype" class="<?php echo getErrMsglabel("pass_re"); ?>">
-                                <input class="password_retype" type="password" name="pass_re" id="password_retype" placeholder="Password retype" value="<?php getFormData("pass_re"); ?>">
+                                <input class="password_retype" type="password" name="pass_re" id="password_retype" placeholder="Password retype" value="<?php echo getFormData("pass_re"); ?>">
                             </label>
                             <div class="help-block"></div>
                             <span class="err_msg"><?php echo getErrMsg("pass_re");  ?>
