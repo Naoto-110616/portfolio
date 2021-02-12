746db64e2fdc66f5c95f2a9bffa889cfd3450d37
busainu <siodeitadaku@gmail.com>
2021-02-12 21:18:19 +0900

    fix mypage postlist function

--- goodbook/mypage.php
+++ goodbook/mypage.php
@@ -57,7 +57,7 @@ if ($_POST["uploadIconImg"] || $_POST["uploadBackgroundImg"]) {
 $dbAreaData = getArea();
 // userのareaを取得
 $dbUserArea = getUserArea($_SESSION["area_id"]);
-
+$dbPostData = getMyPostList();
 $dbFormData = getUser($_SESSION['user_id']);
 
 debug('取得したユーザー情報：' . print_r($dbFormData, true));
@@ -202,38 +202,113 @@ require('goodbook_head.php');
                                 </div>
                             </div>
                         </div>
-                        <div class="share_post">
-                            <div class="share_post_inside">
-                                <div class="share_post_content">
-                                    <div class="share_icon_div">
-                                        <i class="fas fa-user-circle fa-2x"></i>
-                                    </div>
-                                    <div class="share_feelings">
-                                        <h3 class="">Let's share your feelings</h3>
-                                    </div>
-                                </div>
-                                <div class="mypage_border"></div>
-                                <div class="share_list">
-                                    <div class="share_list">
-                                        <div class="">
-                                            <i class="fas fa-video fa-lg"></i>
+                        <div class="myPost">
+                            <div class="share_post">
+                                <div class="share_post_inside">
+                                    <div class="share_post_content">
+                                        <div class="share_icon_div">
+                                            <i class="fas fa-user-circle fa-2x"></i>
                                         </div>
-                                        <p class="">LiveVideo</p>
-                                    </div>
-                                    <div class="share_list">
-                                        <div class="">
-                                            <i class="far fa-images fa-lg"></i>
+                                        <div class="share_feelings">
+                                            <h3 class="">Let's share your feeling</h3>
                                         </div>
-                                        <p class="">Picture&Video</p>
                                     </div>
+                                    <div class="mypage_border"></div>
                                     <div class="share_list">
-                                        <div class="">
-                                            <i class="far fa-smile-wink fa-lg"></i>
+                                        <div class="share_list">
+                                            <div class="">
+                                                <i class="fas fa-video fa-lg"></i>
+                                            </div>
+                                            <p class="">LiveVideo</p>
                                         </div>
-                                        <p class="">Activity</p>
+                                        <div class="share_list">
+                                            <div class="">
+                                                <i class="far fa-images fa-lg"></i>
+                                            </div>
+                                            <p class="">Picture&Video</p>
+                                        </div>
+                                        <div class="share_list">
+                                            <div class="">
+                                                <i class="far fa-smile-wink fa-lg"></i>
+                                            </div>
+                                            <p class="">Activity</p>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
+                            <?php
+                            krsort($dbPostData["data"]);
+                            foreach ($dbPostData['data'] as $key => $val) :
+                            ?>
+                                <section class="main_center_element3">
+                                    <div class="icon_img_div">
+                                        <a href="mypage.php">
+                                            <?php if (empty($dbFormData["profpic"])) { ?>
+                                                <div class="icon_img">
+                                                    <i class="fas fa-user-circle fa-3x"></i>
+                                                </div>
+                                            <?php } else { ?>
+                                                <img class="icon_img" src="<?php echo sanitize($dbFormData["profpic"]) ?>">
+                                            <?php } ?>
+                                        </a>
+                                        <div class="user_name_div">
+                                            <div>
+                                                <p><?php echo userInfoIndicate($dbFormData, "username"); ?></p>
+                                                <p class="time_line"><?php echo mt_rand(1, 24); ?>hour ago</p>
+                                            </div>
+                                        </div>
+                                        <div class="info_list"><i class="fas fa-ellipsis-h"></i></div>
+                                    </div>
+                                    <div>
+                                        <p class="main_center_comment"><?php echo sanitize($val["comment"]); ?></p>
+                                    </div>
+                                    <div class="main_center_mv_div">
+                                        <img class="main_center_img" src="<?php echo sanitize($val['pic1']); ?>" alt="">
+                                    </div>
+                                    <div class="main_center_icon_bottom_div">
+                                        <div class="main_center_icon_bottom">
+                                            <div class="main_center_icon_button_div">
+                                                <img class="main_center_icon_button" src="img/homepage_img/download-2.svg" alt="good_button">
+                                            </div>
+                                            <div class="main_center_icon_button_div">
+                                                <img class="main_center_icon_button" src="img/homepage_img/download-1.svg" alt="love_button">
+                                            </div>
+                                            <div class="main_center_icon_button_div">
+                                                <img class="main_center_icon_button" src="img/homepage_img/download.svg" alt="face_button">
+                                            </div>
+                                            <div class="main_center_icon_button_div">
+                                                <p><?php echo mt_rand(0, 1000); ?></p>
+                                            </div>
+                                        </div>
+                                        <div class="main_center_comment_share_div">
+                                            <p>shere <?php echo mt_rand(0, 100); ?></p>
+                                        </div>
+                                        <div class="border"></div>
+                                        <div class="main_center_element2_share2">
+                                            <div class="main_center_element2_icon_div">
+                                                <div class="main_center_icon main_live_video_icon">
+                                                    <i class="far fa-thumbs-up fa-lg"></i>
+                                                </div>
+                                                <p class="icon_button good_subject">good</p>
+                                            </div>
+                                            <div class="main_center_element2_icon_div">
+                                                <div class="main_center_icon main_video_icon">
+                                                    <i class="far fa-comment fa-lg"></i>
+                                                </div>
+                                                <p class="icon_button comment_subject">comment</p>
+                                            </div>
+                                            <div class="main_center_element2_icon_div">
+                                                <div class="main_center_icon main_activity_icon">
+                                                    <i class="fas fa-reply fa-lg"></i>
+                                                </div>
+                                                <p class="icon_button share_subject">share</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </section>
+                            <?php
+                            endforeach;
+                            ?>
                         </div>
                     </div>
                 </div>
