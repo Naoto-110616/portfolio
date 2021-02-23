09620800cc01585a7548384707e1eca18548755f
busainu <siodeitadaku@gmail.com>
2021-02-23 15:43:25 +0900

    add DM function #36

--- goodbook/msg.php
+++ goodbook/msg.php
@@ -8,9 +8,54 @@ debugLogStart();
 //ログイン認証
 auth();
 
-$u_id = (!empty($_GET['u_id'])) ? $_GET['u_id'] : '';
-$viewData = getUserOne($u_id);
+//================================
+// 画面処理
+//================================
+$partnerUserId = '';
+$partnerUserInfo = '';
+$myUserInfo = '';
+// 画面表示用データ取得
+//================================
+// GETパラメータを取得
+$m_id = (!empty($_GET['m_id'])) ? $_GET['m_id'] : '';
+// DBから掲示板とメッセージデータを取得
+$viewData = getMsgsAndBord($m_id);
+debug('取得したDBデータ：' . print_r($viewData, true));
+// パラメータに不正な値が入っているかチェック
+if (empty($viewData)) {
+    error_log('エラー発生:指定ページに不正な値が入りました');
+    header("Location:mypage.php"); //マイページへ
+}
+// viewDataから相手のユーザーIDを取り出す
+$dealUserIds[] = $viewData[0]['send_user'];
+$dealUserIds[] = $viewData[0]['receive_user'];
+if (($key = array_search($_SESSION['user_id'], $dealUserIds)) !== false) {
+    unset($dealUserIds[$key]);
+}
+debug(print_r($dealUserIds, true));
+$partnerUserId = array_shift($dealUserIds);
+debug('取得した相手のユーザーID：' . $partnerUserId);
+// DBから取引相手のユーザー情報を取得
+if (isset($partnerUserId)) {
+    $partnerUserInfo = getUser($partnerUserId);
+}
+// 相手のユーザー情報が取れたかチェック
+if (empty($partnerUserInfo)) {
+    error_log('エラー発生:相手のユーザー情報が取得できませんでした');
+    header("Location:mypage.php"); //マイページへ
+}
+// DBから自分のユーザー情報を取得
+$myUserInfo = getUser($_SESSION['user_id']);
+debug('取得したユーザデータ：' . print_r($partnerUserInfo, true));
+// 自分のユーザー情報が取れたかチェック
+if (empty($myUserInfo)) {
+    error_log('エラー発生:自分のユーザー情報が取得できませんでした');
+    header("Location:mypage.php"); //マイページへ
+}
+createMsg($m_id, $partnerUserId);
 $dbFormData = getUser($_SESSION['user_id']);
+debug('画面表示処理終了 <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<');
+
 ?>
 
 <?php
@@ -24,94 +69,77 @@ require('goodbook_head.php');
     require('goodbook_header.php');
     ?>
 
+    <p id="js-show-msg" style="display:none;" class="msg-slide">
+        <?php echo getSessionFlash('msg_success'); ?>
+    </p>
+
     <!-- メインコンテンツ -->
     <div id="contents" class="site-width">
         <!-- Main -->
         <section id="main">
             <div class="msg-info">
                 <div class="avatar-img">
-                    <img src="<?php echo sanitize($viewData["profpic"]) ?>" alt="" class="avatar">
+                    <a href="userDetail.php<?php echo (!empty(appendGetParam())) ? appendGetParam() . '&u_id=' . $partnerUserInfo['id'] : '?u_id=' . $partnerUserInfo['id']; ?>" class="">
+                        <img src="<?php echo showImg(sanitize($partnerUserInfo['profpic'])); ?>" alt="" class="avatar"><br>
+                    </a>
                 </div>
                 <div class="avatar-info">
-                    <?php echo sanitize($viewData["username"]) ?><br>
+                    <?php echo sanitize($partnerUserInfo['username']) . ' ' . sanitize($partnerUserInfo['age']) . '歳' ?><br>
+                    〒<?php echo sanitize($partnerUserInfo['zip']); ?><br>
+                    <?php echo sanitize($partnerUserInfo['addr']); ?><br>
+                    TEL：<?php echo sanitize($partnerUserInfo['tel']); ?>
                 </div>
             </div>
             <div class="area-bord" id="js-scroll-bottom">
-                <div class="msg-cnt msg-left">
-                    <div class="avatar">
-                        <img src="<?php echo sanitize($viewData["profpic"]) ?>" alt="" class="avatar">
-                    </div>
-                    <p class="msg-inrTxt">
-                        <span class="triangle"></span>
-                        サンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキスト
-                    </p>
-                </div>
-                <div class="msg-cnt msg-right">
-                    <div class="avatar">
-                        <img src="<?php echo sanitize($dbFormData["profpic"]) ?>" alt="" class="avatar">
-                    </div>
-                    <p class="msg-inrTxt">
-                        <span class="triangle"></span>
-                        サンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキ
-                    </p>
-                </div>
-                <div class="msg-cnt msg-left">
-                    <div class="avatar">
-                        <img src="<?php echo sanitize($viewData["profpic"]) ?>" alt="" class="avatar">
-                    </div>
-                    <p class="msg-inrTxt">
-                        <span class="triangle"></span>
-                        サンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサ
-                    </p>
-                </div>
-                <div class="msg-cnt msg-right">
-                    <div class="avatar">
-                        <img src="<?php echo sanitize($dbFormData["profpic"]) ?>" alt="" class="avatar">
-                    </div>
-                    <p class="msg-inrTxt">
-                        <span class="triangle"></span>
-                        サンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキ
-                    </p>
-                </div>
-                <div class="msg-cnt msg-left">
-                    <div class="avatar">
-                        <img src="<?php echo sanitize($viewData["profpic"]) ?>" alt="" class="avatar">
-                    </div>
-                    <p class="msg-inrTxt">
-                        <span class="triangle"></span>
-                        サンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサ
-                    </p>
-                </div>
-                <div class="msg-cnt msg-right">
-                    <div class="avatar">
-                        <img src="<?php echo sanitize($dbFormData["profpic"]) ?>" alt="" class="avatar">
-                    </div>
-                    <p class="msg-inrTxt">
-                        <span class="triangle"></span>
-                        サンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキ
-                    </p>
-                </div>
-                <div class="msg-cnt msg-left">
-                    <div class="avatar">
-                        <img src="<?php echo sanitize($viewData["profpic"]) ?>" alt="" class="avatar">
-                    </div>
-                    <p class="msg-inrTxt">
-                        <span class="triangle"></span>
-                        サンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサ
-                    </p>
-                </div>
+                <?php
+                if (!empty($viewData[0]["msg"])) {
+                    foreach ($viewData as $key => $val) {
+                        if (!empty($val['from_user']) && $val['from_user'] == $partnerUserId) {
+                ?>
+                            <div class="msg-cnt msg-left">
+                                <div class="avatar">
+                                    <img src="<?php echo sanitize(showImg($partnerUserInfo['profpic'])); ?>" alt="" class="avatar">
+                                </div>
+                                <p class="msg-inrTxt">
+                                    <span class="triangle"></span>
+                                    <?php echo sanitize($val['msg']); ?>
+                                </p>
+                                <div style="font-size:.5em;"><?php echo sanitize($val['send_date']); ?></div>
+                            </div>
+                        <?php
+                        } else {
+                        ?>
+                            <div class="msg-cnt msg-right">
+                                <div class="avatar">
+                                    <img src="<?php echo sanitize(showImg($myUserInfo['profpic'])); ?>" alt="" class="avatar">
+                                </div>
+                                <p class="msg-inrTxt">
+                                    <span class="triangle"></span>
+                                    <?php echo sanitize($val['msg']); ?>
+                                </p>
+                                <div style="font-size:.5em;text-align:right;"><?php echo sanitize($val['send_date']); ?></div>
+                            </div>
+                    <?php
+                        }
+                    }
+                } else { ?>
+                    <p style="text-align:center;line-height:20;">メッセージ投稿はまだありません</p>
+                <?php } ?>
+
             </div>
             <div class="area-send-msg">
-                <textarea name="" id="" cols="30" rows="3"></textarea>
-                <input type="submit" value="送信" class="btn btn-send">
+                <form action="" method="post">
+                    <textarea name="msg" cols="30" rows="3"></textarea>
+                    <input type="submit" value="送信" class="btn btn-send">
+                </form>
             </div>
         </section>
 
-        <script src="js/vendor/jquery-2.2.2.min.js"></script>
+    </div>
 
-        <?php
-        require("jssrc.php");
-        ?>
+    <?php
+    require("jssrc.php");
+    ?>
 
     </div>
 
