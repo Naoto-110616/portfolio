ab45c5242a090f5dee3985dfe8321e5a2ce6ae19
busainu <siodeitadaku@gmail.com>
2021-02-27 19:36:50 +0900

    fix to page url

--- goodbook/goodbook_header.php
+++ goodbook/goodbook_header.php
@@ -16,7 +16,9 @@
             <a class="to_homepage" href="homepage.php"><i class="fas fa-home fa-2x"></i></a>
         </div>
         <div class="header_center_icon">
-            <a href="friendsList.php"><i class=" fas fa-user-friends fa-2x"></i></a>
+            <a href="friendsList.php<?php echo "?u_id=" . $_SESSION["user_id"] ?>">
+                <i class=" fas fa-user-friends fa-2x"></i>
+            </a>
         </div>
         <div class="header_center_icon"><i class="fas fa-tv fa-2x"></i></div>
         <div class="header_center_icon">
