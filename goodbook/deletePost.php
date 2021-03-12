bc7dea0018525a17dc0085ea68622debb974a843
busainu <siodeitadaku@gmail.com>
2021-03-12 15:00:45 +0900

    add delete post function #33

---
+++ goodbook/deletePost.php
@@ -0,0 +1,11 @@
+<?php
+//共通変数・関数ファイルを読込み
+require('function.php');
+
+debug('「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「');
+debug('「　delete post page　');
+debug('「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「');
+debugLogStart();
+
+$p_id = (!empty($_GET['p_id'])) ? $_GET['p_id'] : '';
+deletePost($p_id);
