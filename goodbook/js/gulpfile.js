eae6b744df93afcde973f957b15b09ed2916a201
busainu <siodeitadaku@gmail.com>
2021-03-23 10:57:19 +0900

    add scss file gulp function

---
+++ goodbook/js/gulpfile.js
@@ -0,0 +1,61 @@
+// plug-in
+var gulp = require("gulp");
+var minifycss = require("gulp-minify-css");
+var sass = require("gulp-sass");
+var changed = require("gulp-changed");
+var imagemin = require("gulp-imagemin");
+
+// gulpタスクの作成
+// gulp.task()を使う
+// 第一引数に任意のタスク名、第二引数に実行したい処理をfunction関数で渡してあげる
+// 関数内ではpipe()で処理を繋ぐ(jQueryのメソッドチェーンと同じ)
+
+// CSS圧縮
+gulp.task("minify-css", function () {
+	return gulp
+		.src("../src/css/*.css")
+		.pipe(minifycss())
+		.pipe(gulp.dest("../dist/css/"));
+});
+
+// defaultで動かすタスクを指定
+// defaultに設定しておくとgulpコマンドだけでタスクが実行される
+// 書き方は第二引数に配列でタスクを指定する
+// gulp.task("default",["タスク名","タスク名","タスク名",...]);
+gulp.task("default", gulp.task("minify-css"));
+
+// sassをコンパイル
+gulp.task("sass", function () {
+	return gulp.src("../scss/style.scss").pipe(sass()).pipe(gulp.dest("../dist/css/"));
+});
+
+// 画像圧縮
+// 圧縮前と圧縮後のlディレクトリを定義
+var paths = {
+	srcDir: "src",
+	dstDir: "dist",
+};
+// jpg,png,gif画像の圧縮タスク
+gulp.task("imagemin", function () {
+	var srcGlob = paths.srcDir + "/**/*.+(jpg|jpeg|png|gif)";
+	var dstGlob = paths.dstDir;
+	return gulp
+		.src(srcGlob)
+		.pipe(changed(dstGlob))
+		.pipe(
+			imagemin([
+				imagemin.gifsicle({ interlaced: true }),
+				imagemin.mozjpeg({ progressive: true }),
+				imagemin.optipng({ optimizationLeval: 5 }),
+			])
+		)
+		.pipe(gulp.dest(dstGlob));
+});
+
+// Gulpを使ったファイルの監視
+// watch()を使う
+// 第一引数は監視したいディレクトリ配下
+// 第二引数に変更があった場合に実行するタスクを配列形式で指定
+gulp.task("watch", function () {
+	return gulp.watch(paths.srcDir + "/**/*", gulp.task("imagemin"));
+});
