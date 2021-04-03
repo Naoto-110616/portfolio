4779ace9fd078efcaa7484dfdea0c3171bf8792f
busainu <siodeitadaku@gmail.com>
2021-04-03 20:25:39 +0900

    add css transform functioin

---
+++ Oasobi/Oasobi_homepage.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../reset.css">
+    <link rel="stylesheet" href="../portfolio_style.css">
+    <title>Choice Page</title>
+</head>
+
+<body>
+    <main>
+        <h1 style="text-align: center;">CSS(transform)</h1>
+        <section class="section_oasobi">
+            <a href="oasobi1/index.html">
+                <div class="oasobi_page_name">
+                    <h2>Great man</h2>
+                </div>
+            </a>
+            <a href="oasobi2/index.html">
+                <div class="oasobi_page_name">
+                    <h2>Artist</h2>
+                </div>
+            </a>
+        </section>
+    </main>
+    <?php
+    $href = "../Portfolio.php";
+    require("../home_footer.php"); ?>
+</body>
+
+</html>
