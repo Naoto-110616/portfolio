ef2e843faa785f8545772ae996f74806c5e61cfe
busainu <siodeitadaku@gmail.com>
2021-04-13 18:20:41 +0900

    add responsive design top page

--- home_footer.php
+++ home_footer.php
@@ -1,7 +1,7 @@
 <footer class="footer">
     <div class="content">
         <h2>Portfolio</h2>
-        <p>お時間いただき、ありがとうございました。</p>
+        <div>お時間いただき、ありがとうございました。</div>
         <div>これまでの経験を活かして、業務を進めて参ります。</div>
         <div>よろしくお願いします。</div>
         <p><a href="<?php echo $href ?>">Portfolio トップページへ</a></p>
