5c58e73b178348c36f59cada061aebe389153fc9
busainu <siodeitadaku@gmail.com>
2021-04-16 20:14:18 +0900

    add icon

--- goodbook/head.php
+++ goodbook/head.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>goodbook - <?php echo $siteTitle; ?></title>
+    <link rel="icon" href="img/Facebook-Logo1.png" type="image/jpg">
     <link rel="stylesheet" href="dist/css/reset.css">
     <link rel="stylesheet" href="dist/css/login&signup_style.css">
 </head>
--- home_head.php
+++ home_head.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>Portfolio</title>
+    <link rel="icon" href="img/IMG_2080.jpg" type="image/jpg">
     <link rel="stylesheet" href="dist/css/reset.css">
     <link rel="stylesheet" href="dist/css/portfolio_style.css">
 </head>
