d4b54fe35b7b16c121a83d3a278ed61b67edaa9f
Naoto-110616 <siodeitadaku@gmail.com>
2021-10-20 21:15:52 +0900

    add Footer

--- 20211019/React_app_with_docker/reactapp/src/components/UI/Layouts/Footer/Footer.js
+++ 20211019/React_app_with_docker/reactapp/src/components/UI/Layouts/Footer/Footer.js
@@ -4,6 +4,9 @@ import classes from "./Footer.module.css";
 const Footer = (props) => {
 	return (
 		<footer className={classes.footer}>
+			<div>
+				<p>© Copyright 2021 busainu.com</p>
+			</div>
 			<Icons className={classes.icon} />
 		</footer>
 	);
