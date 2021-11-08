bf2c9e8e35ebc2d72dfb4de2f78f677a558a24a3
Naoto-110616 <siodeitadaku@gmail.com>
2021-11-08 16:57:28 +0900

    fix header

--- 20211019/React_app_with_docker/reactapp/src/components/UI/Layouts/Header/Header.js
+++ 20211019/React_app_with_docker/reactapp/src/components/UI/Layouts/Header/Header.js
@@ -4,17 +4,17 @@ import Icons from "./icons/Icons";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 
 const Header = () => {
-	return (
-		<header className={classes.header}>
-			<AnchorLink href="#root">
-				<h1>Portfolio</h1>
-			</AnchorLink>
-			<div className={classes["header-item"]}>
-				<Icons />
-				<HeaderNav />
-			</div>
-		</header>
-	);
+  return (
+    <header className={classes.header}>
+      <AnchorLink href="#root">
+        <h1>Portfolio</h1>
+      </AnchorLink>
+      <div className={classes["header-item"]}>
+        <Icons />
+        <HeaderNav />
+      </div>
+    </header>
+  );
 };
 
 export default Header;
--- 20211019/React_app_with_docker/reactapp/src/components/UI/Layouts/Header/headerNav/HeaderNav.js
+++ 20211019/React_app_with_docker/reactapp/src/components/UI/Layouts/Header/headerNav/HeaderNav.js
@@ -1,24 +1,30 @@
 import classes from "./HeaderNav.module.css";
 import HeaderNavButton from "./HeaderNavButton";
 import { OFFSET_WORK_LOG, OFFSET_TOOL_SET } from "../../../../../util/consts";
+import MediaQuery from "react-responsive";
 
 const HeaderNav = () => {
-	return (
-		<nav>
-			<ul className={classes["item-list"]}>
-				<HeaderNavButton
-					navItem={"Work Log"}
-					href={"#worklog"}
-					offset={OFFSET_WORK_LOG}
-				/>
-				<HeaderNavButton
-					navItem={"Toolset"}
-					href={"#toolset"}
-					offset={OFFSET_TOOL_SET}
-				/>
-				<HeaderNavButton navItem={"Side Projects"} href={"#side-projects"} />
-			</ul>
-		</nav>
-	);
+  return (
+    <nav>
+      <MediaQuery query="(min-width:768px)">
+        <ul className={classes["item-list"]}>
+          <HeaderNavButton
+            navItem={"Work Log"}
+            href={"#worklog"}
+            offset={OFFSET_WORK_LOG}
+          />
+          <HeaderNavButton
+            navItem={"Toolset"}
+            href={"#toolset"}
+            offset={OFFSET_TOOL_SET}
+          />
+          <HeaderNavButton navItem={"Side Projects"} href={"#side-projects"} />
+        </ul>
+      </MediaQuery>
+      <MediaQuery query="(max-width:767px)">
+		  
+	  </MediaQuery>
+    </nav>
+  );
 };
 export default HeaderNav;
