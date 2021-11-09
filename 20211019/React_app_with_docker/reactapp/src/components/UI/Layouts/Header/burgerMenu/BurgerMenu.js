083cd2eeabd5f13d8e688bfd409112b18db9e7b3
Naoto-110616 <siodeitadaku@gmail.com>
2021-11-09 14:38:26 +0900

    add burger menu

--- 20211019/React_app_with_docker/reactapp/src/components/UI/Layouts/Header/burgerMenu/BurgerMenu.js
+++ 20211019/React_app_with_docker/reactapp/src/components/UI/Layouts/Header/burgerMenu/BurgerMenu.js
@@ -1,10 +1,10 @@
 import classes from "./BurgerMenu";
-import { slide as Menu } from "react-burger-menu";
+import { fallDown as Menu } from "react-burger-menu";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 
 const BurderMenu = () => {
   return (
-    <Menu>
+    <Menu right>
       <AnchorLink>test</AnchorLink>
       <AnchorLink>test2</AnchorLink>
       <AnchorLink>test3</AnchorLink>
