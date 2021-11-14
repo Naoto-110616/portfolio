0310247c81e22225405bddc8e1fb74c8b60f0e69
Naoto-110616 <siodeitadaku@gmail.com>
2021-11-14 20:44:24 +0900

    fix header button id

--- 20211019/React_app_with_docker/reactapp/src/components/UI/Layouts/Header/headerNav/HeaderNav.js
+++ 20211019/React_app_with_docker/reactapp/src/components/UI/Layouts/Header/headerNav/HeaderNav.js
@@ -1,48 +1,48 @@
 import classes from "./HeaderNav.module.css";
 import HeaderNavButton from "./HeaderNavButton";
 import {
-  OFFSET_WORK_LOG,
-  OFFSET_TOOL_SET,
-  OFFSET_SIDE_PROJECTS,
+	OFFSET_WORK_LOG,
+	OFFSET_TOOL_SET,
+	OFFSET_SIDE_PROJECTS,
 } from "../../../../../util/consts";
 
 const HeaderNav = () => {
-  const HEADER_NAV = [
-    {
-      id: "worklog",
-      navItem: "Work Log",
-      href: "#worklog",
-      offset: OFFSET_WORK_LOG,
-    },
-    {
-      id: "toolset",
-      navItem: "Toolset",
-      href: "#toolset",
-      offset: OFFSET_TOOL_SET,
-    },
-    {
-      id: "side-projects",
-      navItem: "Side Projects",
-      href: "#side-projects",
-      offset: OFFSET_SIDE_PROJECTS,
-    },
-  ];
-  const headerNav = HEADER_NAV.map((data) => {
-    return (
-      <HeaderNavButton
-        id={data.id}
-        key={data.id}
-        navItem={data.navItem}
-        href={data.href}
-        offset={data.offset}
-      ></HeaderNavButton>
-    );
-  });
+	const HEADER_NAV = [
+		{
+			id: "header_worklog",
+			navItem: "Work Log",
+			href: "#worklog",
+			offset: OFFSET_WORK_LOG,
+		},
+		{
+			id: "header_toolset",
+			navItem: "Toolset",
+			href: "#toolset",
+			offset: OFFSET_TOOL_SET,
+		},
+		{
+			id: "header_side-projects",
+			navItem: "Side Projects",
+			href: "#side-projects",
+			offset: OFFSET_SIDE_PROJECTS,
+		},
+	];
+	const headerNav = HEADER_NAV.map((data) => {
+		return (
+			<HeaderNavButton
+				id={data.id}
+				key={data.id}
+				navItem={data.navItem}
+				href={data.href}
+				offset={data.offset}
+			></HeaderNavButton>
+		);
+	});
 
-  return (
-    <nav>
-      <ul className={classes["item-list"]}>{headerNav}</ul>
-    </nav>
-  );
+	return (
+		<nav>
+			<ul className={classes["item-list"]}>{headerNav}</ul>
+		</nav>
+	);
 };
 export default HeaderNav;
--- 20211019/React_app_with_docker/reactapp/src/components/UI/Layouts/Header/headerNav/HeaderNavButton.js
+++ 20211019/React_app_with_docker/reactapp/src/components/UI/Layouts/Header/headerNav/HeaderNavButton.js
@@ -3,15 +3,15 @@ import Card from "../../../Card/Card";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 
 const HeaderNavButton = (props) => {
-  return (
-    <AnchorLink
-      id={props.id}
-      key={props.id}
-      href={props.href}
-      offset={props.offset}
-    >
-      <Card className={classes["header-nav-item"]}>{props.navItem}</Card>
-    </AnchorLink>
-  );
+	return (
+		<AnchorLink
+			id={props.id}
+			key={props.id}
+			href={props.href}
+			offset={props.offset}
+		>
+			<Card className={classes["header-nav-item"]}>{props.navItem}</Card>
+		</AnchorLink>
+	);
 };
 export default HeaderNavButton;
