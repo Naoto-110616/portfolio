ed4a9cdc48f6ac23dc36f8d04862c64423148a60
Naoto Okawa <siodeitadaku@gmail.com>
2026-04-06 01:35:36 +0900

    Update HomePage layout for improved spacing and responsiveness
    - Removed unnecessary gap class from the top-level div.
    - Added a responsive gap adjustment for the main section on medium screens to enhance layout consistency across devices.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,10 +12,10 @@ export const revalidate = 300;
 
 export default async function HomePage() {
 	return (
-		<div id="top" className="relative flex flex-col gap-[56px] wide:gap-[80px]">
+		<div id="top" className="relative flex flex-col gap-[56px]">
 			<LayoutGuides lineClassName="bg-foreground/10" />
 			<Header />
-			<main className="mx-auto flex min-h-screen w-full max-w-content flex-col gap-[96px] px-[4.071%] md:px-0 z-20">
+			<main className="mx-auto flex min-h-screen w-full max-w-content flex-col gap-[96px] px-[4.071%] md:px-0 z-20 md:gap-[240px]">
 				<HeroSection />
 				<WorkSection />
 				<MoreProjectsSection />
